3be7a4e90cdc0fa51dc00bbe1b468c2e944f90ab
mahdi-kharrat <mahdikharrat0@gmail.com>
2025-05-29 10:20:46 +0100

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+# app.py
+from flask import Flask, request, jsonify
+import joblib
+import pandas as pd
+
+app = Flask(__name__)
+
+# Charger le modèle
+model = joblib.load('logistic_model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    df = pd.DataFrame([data])
+    prediction = model.predict(df)[0]
+    return jsonify({'prediction': int(prediction)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
